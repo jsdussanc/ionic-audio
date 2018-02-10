97446ee4964b0b555253f1b7cee3c81a35b7bebb
Juan Sebastian Dussan Cubides <cubidesjuan@gmail.com>
2018-02-09 19:20:52 -0500

    recicletracks

--- src/ionic-audio-track-component.ts
+++ src/ionic-audio-track-component.ts
@@ -1,9 +1,9 @@
-import {ITrackConstraint, IAudioTrack, STATUS_MEDIA, Imessage} from './ionic-audio-interfaces'; 
-import {AudioProvider} from './ionic-audio-providers'; 
-import {WebAudioTrack} from './ionic-audio-web-track'; 
-import {CordovaAudioTrack} from './ionic-audio-cordova-track'; 
+import { ITrackConstraint, IAudioTrack, STATUS_MEDIA, Imessage } from './ionic-audio-interfaces';
+import { AudioProvider } from './ionic-audio-providers';
+import { WebAudioTrack } from './ionic-audio-web-track';
+import { CordovaAudioTrack } from './ionic-audio-cordova-track';
 
-import {Component, DoCheck, OnChanges, SimpleChanges, EventEmitter, Output, Input} from '@angular/core';
+import { Component, DoCheck, OnChanges, SimpleChanges, EventEmitter, Output, Input } from '@angular/core';
 
 
 /**
@@ -23,10 +23,10 @@ import {Component, DoCheck, OnChanges, SimpleChanges, EventEmitter, Output, Inpu
  * @class AudioTrackComponent
  */
 @Component({
-    selector: 'audio-track',
-    template: '<ng-content></ng-content>'
+  selector: 'audio-track',
+  template: '<ng-content></ng-content>'
 })
-export class AudioTrackComponent implements OnChanges, DoCheck { 
+export class AudioTrackComponent implements OnChanges, DoCheck {
   /**
    * Input property containing a JSON object with at least a src property
    * ````
@@ -44,7 +44,7 @@ export class AudioTrackComponent implements OnChanges, DoCheck {
   @Input() track: ITrackConstraint;
 
   @Input() autoplay: boolean;
-  
+
   /**
    * Output property expects an event handler to be notified whenever playback finishes
    * 
@@ -52,7 +52,7 @@ export class AudioTrackComponent implements OnChanges, DoCheck {
    * @type {EventEmitter}
    */
   @Output() onFinish = new EventEmitter<ITrackConstraint>();
-  
+
   /**
    * Output property expects an event handler to be notified whenever playback finishes
    * 
@@ -61,11 +61,11 @@ export class AudioTrackComponent implements OnChanges, DoCheck {
    */
 
   @Output() onEventRecibe = new EventEmitter<Imessage>();
-  
+
   private _audioTrack: IAudioTrack;
-  
-  constructor(private _audioProvider: AudioProvider) {}
-  
+
+  constructor(private _audioProvider: AudioProvider) { }
+
   ngOnInit() {
     if (!this.track) return;
 
@@ -83,100 +83,100 @@ export class AudioTrackComponent implements OnChanges, DoCheck {
         this._audioProvider.add(this._audioTrack);
       }
     }
-    
+
     if (this._audioTrack) {
       // update input track parameter with track is so we pass it to WebAudioProvider if needed
       this.track.id = this._audioTrack.id;
     }
   }
-  
-  play() {    
+
+  play() {
     if (!this._audioTrack) return;
 
     this._audioTrack.play();
     this._audioProvider.current = this._audioTrack.id;
   }
-  
+
   pause() {
     if (!this._audioTrack) return;
 
     this._audioTrack.pause();
     this._audioProvider.current = undefined;
   }
-  
+
   toggle() {
     if (this._audioTrack.isPlaying) {
       this.pause();
     } else {
       this.play();
-    }  
+    }
   }
-  
-  seekTo(time:number) {
+
+  seekTo(time: number) {
     if (!this._audioTrack) return;
 
-    this._audioTrack.seekTo(time);  
+    this._audioTrack.seekTo(time);
   }
-  
-  
-  public get id() : number {
+
+
+  public get id(): number {
     return this._audioTrack ? this._audioTrack.id : -1;
   }
-  
-  public get art() : string {
+
+  public get art(): string {
     return this.track ? this.track.art : undefined;
   }
-  
-  
-  public get artist() : string {
+
+
+  public get artist(): string {
     return this.track ? this.track.artist : undefined;
   }
-  
-  
-  public get title() : string {
+
+
+  public get title(): string {
     return this.track ? this.track.title : undefined;
   }
-    
-  public get progress() : number {
+
+  public get progress(): number {
     return this._audioTrack ? this._audioTrack.progress : 0;
   }
-      
-  public get isPlaying() : boolean {
+
+  public get isPlaying(): boolean {
     return this._audioTrack && this._audioTrack.isPlaying;
   }
 
-  public get isFinished() : boolean {
+  public get isFinished(): boolean {
     return this._audioTrack && this._audioTrack.isFinished;
   }
-  
-  public get duration() : number {
+
+  public get duration(): number {
     return this._audioTrack ? this._audioTrack.duration : 0;
   }
-  
-  public get completed() : number {
+
+  public get completed(): number {
     return this._audioTrack ? this._audioTrack.completed : 0;
   }
-  
+
   public get canPlay() {
     return this._audioTrack && this._audioTrack.canPlay;
   }
-  
+
   public get error() {
     return this._audioTrack ? this._audioTrack.error : undefined;
   }
-  
-  public get isLoading() : boolean {
+
+  public get isLoading(): boolean {
     return this._audioTrack && this._audioTrack.isLoading;
   }
-  
-  public get hasLoaded() : boolean {
+
+  public get hasLoaded(): boolean {
     return this._audioTrack && this._audioTrack.hasLoaded;
   }
-  
+
   ngDoCheck() {
     // track has stopped, trigger finish event
     if (this._audioTrack && this._audioTrack.isFinished) {
-      this.onFinish.emit(this.track);       
+      this.onFinish.emit(this.track);
     }
   }
 
@@ -187,7 +187,11 @@ export class AudioTrackComponent implements OnChanges, DoCheck {
     if (trackPlaying && trackPlaying.src == changes.track.currentValue.src) return; // prevent play again
     if (this._audioTrack && this._audioTrack.isPlaying) this._audioTrack.stop();
     if (changes.track && changes.track.currentValue && changes.track.currentValue.src) {
-      this._audioTrack = this._audioProvider.create(changes.track.currentValue);
+      let trackLoaded = this._audioProvider.tracks.find((item) => { return item.src == changes.track.currentValue.src })
+      if (trackLoaded)
+        this._audioTrack = trackLoaded;
+      else
+        this._audioTrack = this._audioProvider.create(changes.track.currentValue);
       this._audioTrack.subscribe().subscribe(
         (value) => { console.log("AudioTrackComponent:subscribe:onNext:", value); this.onEventRecibe.emit(value) },
         (error) => { console.log("AudioTrackComponent:subscribe:onError:", error) },
